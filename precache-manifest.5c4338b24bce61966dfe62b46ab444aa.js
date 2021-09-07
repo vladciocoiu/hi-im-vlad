d7b21f0381208b9d4fd238d2444d81dfcb96829a
Ciorap88 <vlad.014916@gmail.com>
2021-09-07 14:40:21 +0300

    Updates

---
+++ precache-manifest.5c4338b24bce61966dfe62b46ab444aa.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "de7f2205915295431cf6c39a72adb123",
+    "url": "/hi-im-vlad/index.html"
+  },
+  {
+    "revision": "83f5261c88d98da56e17",
+    "url": "/hi-im-vlad/static/css/main.777afa24.chunk.css"
+  },
+  {
+    "revision": "6c351c0d6322b5b95fa6",
+    "url": "/hi-im-vlad/static/js/2.d85ce09b.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/hi-im-vlad/static/js/2.d85ce09b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "83f5261c88d98da56e17",
+    "url": "/hi-im-vlad/static/js/main.464eb9ca.chunk.js"
+  },
+  {
+    "revision": "db17ad61cb6a84e91be7",
+    "url": "/hi-im-vlad/static/js/runtime-main.01eeef9f.js"
+  },
+  {
+    "revision": "cec48716468b8437167dc1e546c1d0bc",
+    "url": "/hi-im-vlad/static/media/chessEngine.cec48716.png"
+  },
+  {
+    "revision": "3a6b6a19f6ca19187414eeb7a8df8faf",
+    "url": "/hi-im-vlad/static/media/pathfindingBig.3a6b6a19.png"
+  },
+  {
+    "revision": "00866f3b1ee7542c3335b4d86ac77013",
+    "url": "/hi-im-vlad/static/media/sudokuBig.00866f3b.png"
+  }
+]);
